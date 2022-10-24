c2387ebdf0db8aa82cbe9d7eaf86b27c863f33a1
urmilkalaria <urmilkalaria@gmail.com>
2022-10-24 20:22:08 +0530

    fix-broken

--- UK_Profile/urls.py
+++ UK_Profile/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import re_path
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
